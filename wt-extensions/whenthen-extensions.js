4f7a76d6f1edc51c7564dc5f2419c1620ba4f7d4
Mike <mike@whenthen.co>
2015-02-16 21:59:37 +0000

    more styling

--- wt-extensions/whenthen-extensions.js
+++ wt-extensions/whenthen-extensions.js
@@ -839,6 +839,34 @@ Blockly.BlockSvg.prototype.renderDrawRight_ = function(steps, highlightSteps,
 
 
 /**
+ * Add highlighting around this connection.
+ No Change - just moved to pick up the redefined values
+ */
+Blockly.Connection.prototype.highlight = function() {
+  var steps;
+  if (this.type == Blockly.INPUT_VALUE || this.type == Blockly.OUTPUT_VALUE) {
+    var tabWidth = Blockly.RTL ? -Blockly.BlockSvg.TAB_WIDTH :
+                                 Blockly.BlockSvg.TAB_WIDTH;
+    steps = 'm 0,0 v 5 c 0,10 ' + -tabWidth + ',-8 ' + -tabWidth + ',7.5 s ' +
+            tabWidth + ',-2.5 ' + tabWidth + ',7.5 v 5';
+  } else {
+    if (Blockly.RTL) {
+      steps = 'm 20,0 h -5 ' + Blockly.BlockSvg.NOTCH_PATH_RIGHT + ' h -5';
+    } else {
+      steps = 'm -20,0 h 5 ' + Blockly.BlockSvg.NOTCH_PATH_LEFT + ' h 5';
+    }
+  }
+  var xy = this.sourceBlock_.getRelativeToSurfaceXY();
+  var x = this.x_ - xy.x;
+  var y = this.y_ - xy.y;
+  Blockly.Connection.highlightedPath_ = Blockly.createSvgElement('path',
+      {'class': 'blocklyHighlightedConnectionPath',
+       'd': steps,
+       transform: 'translate(' + x + ', ' + y + ')'},
+      this.sourceBlock_.getSvgRoot());
+};
+
+/**
  * Close tooltips, context menus, dropdown selections, etc.
  * @param {boolean=} opt_allowToolbox If true, don't close the toolbox.
  */
